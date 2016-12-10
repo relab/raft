d2f0dff436570b4eaac9181bdc899da31a3f462f
Sebastian M. Pedersen <sebastian@code.64b.io>
2016-12-10 14:48:00 +0100

    Should be milliseconds

--- replica.go
+++ replica.go
@@ -27,7 +27,7 @@ func trace(s string) (string, time.Time) {
 
 func un(s string, startTime time.Time) {
 	endTime := time.Now()
-	log.Println("END:", s, "ElapsedTime in seconds:", endTime.Sub(startTime)/1e3)
+	log.Println("END:", s, "ElapsedTime in milliseconds:", endTime.Sub(startTime)/1e3)
 }
 
 // State represents one of the Raft server states.
