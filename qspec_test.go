0cc1ff0881b060d0ac9b81315cae5b7de7808479
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-01-25 09:03:51 +0100

    qspec_test.go: Refactor tests

--- qspec_test.go
+++ qspec_test.go
@@ -11,6 +11,7 @@ import (
 
 var requestVoteQFTests = []struct {
 	name    string
+	qs      spec
 	request *pb.RequestVoteRequest
 	replies []*pb.RequestVoteResponse
 	quorum  bool
@@ -18,6 +19,7 @@ var requestVoteQFTests = []struct {
 }{
 	{
 		"do not grant vote",
+		n3q1,
 		&pb.RequestVoteRequest{Term: 2},
 		[]*pb.RequestVoteResponse{
 			{Term: 2, VoteGranted: false},
@@ -27,6 +29,7 @@ var requestVoteQFTests = []struct {
 	},
 	{
 		"grant vote",
+		n3q1,
 		&pb.RequestVoteRequest{Term: 3},
 		[]*pb.RequestVoteResponse{
 			{Term: 3, VoteGranted: true},
@@ -36,6 +39,7 @@ var requestVoteQFTests = []struct {
 	},
 	{
 		"reply with higher Term",
+		n3q1,
 		&pb.RequestVoteRequest{Term: 3},
 		[]*pb.RequestVoteResponse{
 			{Term: 4, VoteGranted: false},
@@ -45,17 +49,17 @@ var requestVoteQFTests = []struct {
 	},
 }
 
-type AETestCase struct {
+var appendEntriesQFTests = []struct {
 	name    string
+	qs      spec
 	request *pb.AppendEntriesRequest
 	replies []*pb.AppendEntriesResponse
 	quorum  bool
 	reply   *pb.AppendEntriesResponse
-}
-
-var appendEntriesCommonQFTests = []AETestCase{
+}{
 	{
 		"reply with higher Term",
+		n3q1,
 		&pb.AppendEntriesRequest{Term: 5},
 		[]*pb.AppendEntriesResponse{
 			{
@@ -69,6 +73,7 @@ var appendEntriesCommonQFTests = []AETestCase{
 	},
 	{
 		"one unsuccessful MatchIndex",
+		n3q1,
 		&pb.AppendEntriesRequest{Term: 5},
 		[]*pb.AppendEntriesResponse{
 			{
@@ -86,6 +91,7 @@ var appendEntriesCommonQFTests = []AETestCase{
 	},
 	{
 		"two unsuccessful same MatchIndex",
+		n3q1,
 		&pb.AppendEntriesRequest{Term: 5},
 		[]*pb.AppendEntriesResponse{
 			{
@@ -108,6 +114,7 @@ var appendEntriesCommonQFTests = []AETestCase{
 	},
 	{
 		"two unsuccessful different MatchIndex",
+		n3q1,
 		&pb.AppendEntriesRequest{Term: 5},
 		[]*pb.AppendEntriesResponse{
 			{
@@ -128,43 +135,15 @@ var appendEntriesCommonQFTests = []AETestCase{
 			Success:    false,
 		},
 	},
-}
-
-var appendEntriesFastQFTests = []AETestCase{
-	// Stops after successful quorum, ignoring the last response.
 	{
 		"quorum successful",
+		n3q1,
 		&pb.AppendEntriesRequest{Term: 5},
 		[]*pb.AppendEntriesResponse{
 			{
 				Term:       5,
 				MatchIndex: 100,
 				Success:    true,
-			},
-		},
-		true,
-		&pb.AppendEntriesResponse{
-			Term:       5,
-			MatchIndex: 100,
-			Success:    true,
-		},
-	},
-	// Stops after successful quorum, ignoring the last response.
-	// This tests shows how one follower is left behind, as from this point
-	// it will never receive updates as long as the first follower is faster.
-	{
-		"one of two successful MatchIndex",
-		&pb.AppendEntriesRequest{Term: 5},
-		[]*pb.AppendEntriesResponse{
-			{
-				Term:       5,
-				MatchIndex: 100,
-				Success:    true,
-			},
-			{
-				Term:       5,
-				MatchIndex: 50,
-				Success:    false,
 			},
 		},
 		true,
@@ -176,48 +155,58 @@ var appendEntriesFastQFTests = []AETestCase{
 	},
 }
 
-var qspecs = []struct {
+type spec struct {
 	name string
-	spec gorums.QuorumSpec
-}{
-	{
+	qs   gorums.QuorumSpec
+}
+
+var (
+	n1q1 = spec{
+		"QuorumSpec N1 Q1",
+		raft.NewQuorumSpec(3),
+	}
+	n2q1 = spec{
+		"QuorumSpec N2 Q1",
+		raft.NewQuorumSpec(3),
+	}
+	n3q1 = spec{
 		"QuorumSpec N3 Q1",
 		raft.NewQuorumSpec(3),
-	},
-}
+	}
+	n7q4 = spec{
+		"QuorumSpec N7 Q4",
+		raft.NewQuorumSpec(3),
+	}
+)
 
 func TestRequestVoteQF(t *testing.T) {
-	for _, qs := range qspecs {
-		for _, test := range requestVoteQFTests {
-			t.Run(qs.name+"-"+test.name, func(t *testing.T) {
-				reply, quorum := qs.spec.RequestVoteQF(test.request, test.replies)
+	for _, test := range requestVoteQFTests {
+		t.Run(test.qs.name+"-"+test.name, func(t *testing.T) {
+			reply, quorum := test.qs.qs.RequestVoteQF(test.request, test.replies)
 
-				if quorum != test.quorum {
-					t.Errorf("got %t, want %t", quorum, test.quorum)
-				}
+			if quorum != test.quorum {
+				t.Errorf("got %t, want %t", quorum, test.quorum)
+			}
 
-				if !reflect.DeepEqual(reply, test.reply) {
-					t.Errorf("got %+v, want %+v", reply, test.reply)
-				}
-			})
-		}
+			if !reflect.DeepEqual(reply, test.reply) {
+				t.Errorf("got %+v, want %+v", reply, test.reply)
+			}
+		})
 	}
 }
 
 func TestAppendEntriesFastQF(t *testing.T) {
-	for _, qs := range qspecs {
-		for _, test := range append(appendEntriesCommonQFTests, appendEntriesFastQFTests...) {
-			t.Run(qs.name+"-"+test.name, func(t *testing.T) {
-				reply, quorum := qs.spec.AppendEntriesQF(test.request, test.replies)
+	for _, test := range appendEntriesQFTests {
+		t.Run(test.qs.name+"-"+test.name, func(t *testing.T) {
+			reply, quorum := test.qs.qs.AppendEntriesQF(test.request, test.replies)
 
-				if quorum != test.quorum {
-					t.Errorf("got %t, want %t", quorum, test.quorum)
-				}
+			if quorum != test.quorum {
+				t.Errorf("got %t, want %t", quorum, test.quorum)
+			}
 
-				if !reflect.DeepEqual(reply, test.reply) {
-					t.Errorf("got %+v, want %+v", reply, test.reply)
-				}
-			})
-		}
+			if !reflect.DeepEqual(reply, test.reply) {
+				t.Errorf("got %+v, want %+v", reply, test.reply)
+			}
+		})
 	}
 }
