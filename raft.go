85bcaeda517a8713613d420968b638c813d0a2be
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-02-15 07:46:51 +0100

    raft.go: Update Read comment

--- raft.go
+++ raft.go
@@ -27,9 +27,12 @@ type Raft interface {
 
 	// Read blocks until Raft has had a successful round of heartbeats in
 	// its current term, and the read index is read from Committed, i.e.,
-	// Read must not block reads on Committed. If the context is canceled,
-	// i.e., server is busy, and error is returned and Read must be retired.
-	// Immediately returns an ErrNotLeader error if server isn't the leader.
+	// Read must not block reads on Committed. A successful call to Read
+	// allows read-only queries accumulated up to the point where Read was
+	// called, to be safely executed on the state machine. If the context is
+	// canceled, i.e., server is busy, and error is returned and Read must
+	// be retried. Immediately returns an ErrNotLeader error if server isn't
+	// the leader.
 	Read(context.Context) error
 
 	// ProposeConf proposes a new configuration. Blocks until Raft handles
