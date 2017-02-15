ddd2949de100a3fb19d80793f5a0b087225e22f8
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-02-15 11:47:11 +0100

    raftgorums/raft.go: Fix node addresses

--- raftgorums/raft.go
+++ raftgorums/raft.go
@@ -122,17 +122,6 @@ func NewRaft(cfg *Config) *Raft {
 		cfg.Logger = log.New(ioutil.Discard, "", 0)
 	}
 
-	var nodes []string
-
-	// Remove self
-	for _, node := range cfg.Nodes {
-		if node == cfg.Nodes[cfg.ID-1] {
-			continue
-		}
-
-		nodes = append(nodes, node)
-	}
-
 	term, err := cfg.Storage.Get(KeyTerm)
 
 	if err != nil {
@@ -156,7 +145,7 @@ func NewRaft(cfg *Config) *Raft {
 		votedFor:         votedFor,
 		storage:          cfg.Storage,
 		batch:            cfg.Batch,
-		addrs:            nodes,
+		addrs:            cfg.Nodes,
 		nextIndex:        1,
 		baselineTimeout:  cfg.ElectionTimeout,
 		electionTimeout:  electionTimeout,
@@ -408,7 +397,7 @@ func (r *Raft) Read(ctx context.Context) error {
 	r.Lock()
 	state := r.state
 	leader := r.leader
-	leaderAddr := r.addrs[leader]
+	leaderAddr := r.addrs[leader-1]
 	r.Unlock()
 
 	if state != Leader {
@@ -422,7 +411,7 @@ func (r *Raft) ProposeCmd(ctx context.Context, cmd []byte) error {
 	r.Lock()
 	state := r.state
 	leader := r.leader
-	leaderAddr := r.addrs[leader]
+	leaderAddr := r.addrs[leader-1]
 	term := r.currentTerm
 	r.Unlock()
 
