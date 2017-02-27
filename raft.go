7aeed52376306df21fc8df65c3d5f0660b743096
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-02-27 11:22:53 +0100

    raft: Move Future to its own file

--- raft.go
+++ raft.go
@@ -52,38 +52,3 @@ type Raft interface {
 }
 
 type TODOConfChange struct{}
-
-// Future allows a result to be read after the operation who created it has
-// completed.
-type Future interface {
-	// Must not be called until Result has been read.
-	Index() uint64
-	Result() interface{}
-}
-
-type EntryFuture struct {
-	Entry *commonpb.Entry
-
-	Res  interface{}
-	done chan struct{}
-}
-
-func NewFuture(entry *commonpb.Entry) *EntryFuture {
-	return &EntryFuture{
-		Entry: entry,
-		done:  make(chan struct{}),
-	}
-}
-
-func (f *EntryFuture) Index() uint64 {
-	return f.Entry.Index
-}
-
-func (f *EntryFuture) Result() interface{} {
-	<-f.done
-	return f.Res
-}
-
-func (f *EntryFuture) Respond() {
-	close(f.done)
-}
