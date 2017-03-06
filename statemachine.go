0568d22202e1b1273470175235e3fbef66aa1c13
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-03-06 11:55:06 +0100

    statemachine.go: Add comment about concurrency

--- statemachine.go
+++ statemachine.go
@@ -3,6 +3,8 @@ package raft
 import commonpb "github.com/relab/raft/raftpb"
 
 // StateMachine provides an interface for state machines using the Raft log.
+// Raft will not call any of these methods concurrently, i.e., your StateMachine
+// implementation does not need to be thread-safe.
 type StateMachine interface {
 	Apply(*commonpb.Entry) interface{}
 
