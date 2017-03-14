5f0a82366986641054f984bf82a7b00733604290
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-03-14 11:20:17 +0100

    raftgorums: Switch AppendEntries to per_node_arg

--- raftgorums/node.go
+++ raftgorums/node.go
@@ -120,7 +120,11 @@ func (n *Node) Run() error {
 
 		case req := <-n.Raft.aereqout:
 			ctx, cancel := context.WithTimeout(context.Background(), TCPHeartbeat*time.Millisecond)
-			res, err := n.conf.AppendEntries(ctx, req)
+			res, err := n.conf.AppendEntries(ctx, req,
+				func(req pb.AppendEntriesRequest, nodeID uint32) *pb.AppendEntriesRequest {
+					return &req
+				},
+			)
 
 			if err != nil {
 				n.logger.WithError(err).Warnln("AppendEntries failed")
