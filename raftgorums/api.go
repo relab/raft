fb9bbb8b162de3d8a51f35f949664bea76e681dc
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-04-12 18:46:36 +0200

    raft,commonpb,raftgorums: Update shared reconfiguration proto types

--- raftgorums/api.go
+++ raftgorums/api.go
@@ -8,8 +8,8 @@ import (
 )
 
 // ProposeConf implements raft.Raft.
-func (r *Raft) ProposeConf(ctx context.Context, confChange *commonpb.ConfChangeRequest) (raft.Future, error) {
-	cmd, err := confChange.Marshal()
+func (r *Raft) ProposeConf(ctx context.Context, req *commonpb.ReconfRequest) (raft.Future, error) {
+	cmd, err := req.Marshal()
 
 	if err != nil {
 		return nil, err
@@ -19,21 +19,21 @@ func (r *Raft) ProposeConf(ctx context.Context, confChange *commonpb.ConfChangeR
 
 	if err != nil {
 		err := err.(raft.ErrNotLeader)
-		future.Respond(&commonpb.ConfChangeResponse{
-			Status:     commonpb.ConfNotLeader,
+		future.Respond(&commonpb.ReconfResponse{
+			Status:     commonpb.ReconfNotLeader,
 			LeaderHint: r.addrs[err.Leader-1],
 		})
 		return future, nil
 	}
 
 	if !r.allowReconfiguration() {
-		future.Respond(&commonpb.ConfChangeResponse{
-			Status: commonpb.ConfTimeout,
+		future.Respond(&commonpb.ReconfResponse{
+			Status: commonpb.ReconfTimeout,
 		})
 		return future, nil
 	}
 
-	go r.replicate(confChange.ServerID, future)
+	go r.replicate(req.ServerID, future)
 
 	return future, nil
 }
