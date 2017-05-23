d7c3b9f63ff56099affe763a5d9ddfdc601a7bd9
Sebastian M. Pedersen <sebastian@code.64b.io>
2017-05-23 11:39:09 +0200

    raftgorums: Fix check quorum not enabled in integration tests

--- raftgorums/integration_test.go
+++ raftgorums/integration_test.go
@@ -116,6 +116,7 @@ func newTestServer(t *testing.T, wg *sync.WaitGroup, c *cfg, port uint64, exclud
 		Storage:          server.mem,
 		HeartbeatTimeout: heartbeat,
 		ElectionTimeout:  c.electionTimeout,
+		CheckQuorum:      true,
 		Logger: &logrus.Logger{
 			Out: ioutil.Discard,
 		},
